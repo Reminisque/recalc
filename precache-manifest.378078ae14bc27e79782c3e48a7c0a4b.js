5c6bd81079525f25a0744934b4c834bea304014b
Reminisque <travisc2728@gmail.com>
2020-05-22 16:57:17 -0700

    Updates

---
+++ precache-manifest.378078ae14bc27e79782c3e48a7c0a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aea1044eedf5e69d6c54d33a6fca803",
+    "url": "/recalc/index.html"
+  },
+  {
+    "revision": "a804500d135ffc7639ff",
+    "url": "/recalc/static/css/main.70a6fd3d.chunk.css"
+  },
+  {
+    "revision": "e4f29c8ef71a6d382ade",
+    "url": "/recalc/static/js/2.8181f1ac.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recalc/static/js/2.8181f1ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a804500d135ffc7639ff",
+    "url": "/recalc/static/js/main.a9347957.chunk.js"
+  },
+  {
+    "revision": "b36b52319cd61df04b07",
+    "url": "/recalc/static/js/runtime-main.0d028ab9.js"
+  }
+]);
